55f419b707df65edd2c128ece17e113dd973115e
Henry Adagala <adagalahenry@gmail.com>
2022-03-18 18:08:56 +0300

    add: fit bounds as per array of geocodes received

--- src/app/map/components/map/map.component.ts
+++ src/app/map/components/map/map.component.ts
@@ -3,7 +3,6 @@ import { Store } from '@ngrx/store';
 import * as mapboxgl from 'mapbox-gl';
 import { map, mergeMap, Subscription } from 'rxjs';
 import * as MapActions from '../../actions';
-import { IGeocode } from '../../models';
 import * as MapSelectors from '../../selectors';
 
 @Component({
@@ -14,33 +13,43 @@ import * as MapSelectors from '../../selectors';
 export class MapComponent implements OnInit, OnDestroy {
   private accessToken = 'pk.eyJ1IjoiYWRhZ2FsYSIsImEiOiJjbDB2ZnRnb2EwMXdjM2ptdDhwa3NxODA1In0.iJDvbyay4kzgGsNAgb3hAw';
   private records: Subscription | undefined;
+  private boundsSubscription: Subscription | undefined;
 
   constructor(private store: Store) {
     this.store.dispatch(MapActions.loadListings());
   }
 
   ngOnInit(): void {
-
     const mapboxglMap = new mapboxgl.Map({
       accessToken: this.accessToken,
       container: 'map', // container ID
       style: 'https://api.maptiler.com/maps/openstreetmap/style.json?key=SoL71Zyf7SmLrVYWC7fQ', // style URL
-      center: [-95.601961, 29.718855],
       zoom: 12
     });
 
-    this.records = this.store.select(MapSelectors.selectAll)
-      .pipe(mergeMap(records => records), map(recode => recode.geocode))
-      .subscribe(geoCode => {
-        const longitude = +geoCode.Longitude;
-        const latitude = +geoCode.Latitude;
+    this.records = this.store.select(MapSelectors.selectGeocodes)
+      .pipe(
+        mergeMap(geocodes => geocodes),
+        map(geocode => geocode))
+      .subscribe(geocode => {
+        const longitude = +geocode.Longitude;
+        const latitude = +geocode.Latitude;
         new mapboxgl.Marker({ color: 'pink' })
           .setLngLat([longitude, latitude])
           .addTo(mapboxglMap);
       });
+
+
+    this.boundsSubscription = this.store.select(MapSelectors.selectBounds)
+      .subscribe(bounds => {
+        mapboxglMap.fitBounds(bounds, {
+          padding: 100
+        });
+      })
   }
 
   ngOnDestroy(): void {
     this.records?.unsubscribe();
+    this.boundsSubscription?.unsubscribe();
   }
 }
--- src/app/map/selectors/map.selectors.ts
+++ src/app/map/selectors/map.selectors.ts
@@ -18,8 +18,29 @@ export const {
     selectTotal
 } = fromMaps.mapAdapter.getSelectors(selectEntitiesState);
 
-export const selectSelectedEmployee = createSelector(
+export const selectSelectedEntity = createSelector(
     selectEntities,
     selectSelectedId,
     (entities, selectedId) => selectedId && entities[selectedId]
 );
+
+export const selectBounds = createSelector(selectAll, state => {
+    const longitudes = state.map(records => +records.geocode.Longitude);
+    const latitudes = state.map(records => +records.geocode.Latitude);
+
+    const minLatitude = latitudes.length > 0 ? Math.min(...latitudes) : 0;
+    const minLongitude = longitudes.length > 0 ? Math.min(...longitudes) : 0;
+    const southWesternBound: mapboxgl.LngLatLike = [minLongitude, minLatitude];
+
+    const maxLatitude = latitudes.length > 0 ? Math.max(...latitudes) : 0;
+    const maxLongitude = longitudes.length > 0 ? Math.max(...longitudes) : 0;
+    const northEasternBound: mapboxgl.LngLatLike = [maxLongitude, maxLatitude];
+
+    const bounds: mapboxgl.LngLatBoundsLike = [southWesternBound, northEasternBound];
+    return bounds;
+});
+
+export const selectGeocodes = createSelector(selectAll, state => {
+    const geocodes = state.map(records => records.geocode);
+    return geocodes;
+});
